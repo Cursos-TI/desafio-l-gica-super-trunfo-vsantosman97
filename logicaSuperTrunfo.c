4a782ccbaf0d0525b2e92e54b5565dcd187d5774
vsantosman97 <202501696406@alunos.estacio.br>
2025-04-25 17:42:49 +0000

    Criado variavel estado

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,6 +6,8 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+
+    int estado;
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
